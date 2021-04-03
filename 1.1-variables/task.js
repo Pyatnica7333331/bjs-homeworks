96929150ab93fd84f54393412b29796b5825cb6c
Pyatnica7333331 <alferova7333331@gmail.com>
2021-04-03 12:47:18 +0300

    First commit

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,24 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	average = (algebra + geography + physics) / 3;
+   
+	return average;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	return `Привет, мир! Меня зовут ${userName}`;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    let x = 2;
+	let y = 22;
+	let z = 0;
+	result = x * y + 5 * z + x - 1;
+	    
+    return result;
 }
