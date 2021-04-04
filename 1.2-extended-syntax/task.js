95a0d8ed76f8bfcf712291dafa74d3f65afb7f53
Pyatnica7333331 <alferova7333331@gmail.com>
2021-04-04 22:14:15 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let x = [];
+    let d = b ** 2 - 4 * a * c;
+    if (d < 0) {
+       x = [];
+    } else if (d == 0) {
+       x[0] = -b / (2 *a);
+       
+    } else {
+        x[0] = (-b + Math.sqrt(d)) / (2 *a);
+        x[1] = (-b - Math.sqrt(d)) / (2 *a);
+        
+    };
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    let averageMark;
+    
+    if (marks.length > 5) {
+        console.log("Оценок очень много, считаются первые 5 штук");
+        marks = marks.slice(0, 5);
+    };     
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    };
+   
+    averageMark = sum / marks.length;
+
+    return (averageMark || 0);
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let result = "";
+    let age = 2021 - dateOfBirthday.getFullYear();
+
+    if (age >= 18) {
+      result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else if (age < 18) {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+    };
+
+    return result;
 }
