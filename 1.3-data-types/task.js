458505238c1a285f7a524d7f073433e9fa0cab69
Pyatnica7333331 <alferova7333331@gmail.com>
2021-04-11 18:06:31 +0300

    First commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+    if (typeof percent != "number") {
+      return `Параметр Процентная ставка содержит неправильное значение ${percent}`;
+    } else if (typeof contribution != "number") {
+        return `Параметр Начальный взнос содержит неправильное значение ${contribution}`;
+    } else if (typeof amount != "number") {
+        return `Параметр Общая стоимость содержит неправильное значение ${amount}`;
+    };
+    
+    let bodyOfMortgage = amount - contribution;
+    let duration = (date.getFullYear() -new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let P = percent / 100 / 12;
+    let mounthPayment = bodyOfMortgage * (P + P / (Math.pow(1 + P, duration) - 1));
+    let totalAmount = mounthPayment * duration;
+    
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if (name != false & name != undefined) {
+      return `Привет, мир! Меня зовут ${name}.`;
+    } else {
+      return "Привет, мир! Меня зовут Аноним.";
+    };
+    
 }
