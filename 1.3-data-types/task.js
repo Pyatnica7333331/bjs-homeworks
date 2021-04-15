d23911369d9e2b7743691144ea04cea2d097b672
Pyatnica7333331 <alferova7333331@gmail.com>
2021-04-15 23:49:31 +0300

    finalized task1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,28 +1,31 @@
 "use strict";
-function calculateTotalMortgage(percent, contribution, amount, date) {
-    percent = Number(percent);
-    contribution = Number(contribution);
-    amount = Number(amount);
-    if (typeof percent != "number") {
-      return `Параметр Процентная ставка содержит неправильное значение ${percent}`;
-    } else if (typeof contribution != "number") {
-        return `Параметр Начальный взнос содержит неправильное значение ${contribution}`;
-    } else if (typeof amount != "number") {
-        return `Параметр Общая стоимость содержит неправильное значение ${amount}`;
+function calculateTotalMortgage(percentInput, contributionInput, amountInput, date) {
+    let percent = Number(percentInput);
+    let contribution =  Number(contributionInput);
+    let amount = Number(amountInput);
+
+    if (Number.isNaN(percent)) {
+      return `Параметр Процентная ставка содержит неправильное значение ${percentInput}`;
+    } else if (Number.isNaN(contribution)) {
+        return `Параметр Начальный взнос содержит неправильное значение ${contributionInput}`;
+    } else if (Number.isNaN(amount)) {
+        return `Параметр Общая стоимость содержит неправильное значение ${amountInput}`;
     };
-    
-    let bodyOfMortgage = amount - contribution;
     let duration = (date.getFullYear() -new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
-    let P = percent / 100 / 12;
-    let mounthPayment = bodyOfMortgage * (P + P / (Math.pow(1 + P, duration) - 1));
-    let totalAmount = mounthPayment * duration;
     
-    return Number(totalAmount.toFixed(2));
+    if ((percent > 0) && (contribution >= 0) && (amount > 0) && (duration > 0)) {
+      let bodyOfMortgage = amount - contribution;
+      let P = percent / 100 / 12;
+      let mounthPayment = bodyOfMortgage * (P + P / (Math.pow(1 + P, duration) - 1));
+      let totalAmount = mounthPayment * duration;
+      return Number(totalAmount.toFixed(2));
+    } else 
+      return "Ошибка введенных значений";
 }
 
 function getGreeting(name) {
     
-    if (name != false & name != undefined) {
+    if (name) {
       return `Привет, мир! Меня зовут ${name}.`;
     } else {
       return "Привет, мир! Меня зовут Аноним.";
